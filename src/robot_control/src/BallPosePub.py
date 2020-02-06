Corrected calculation for BallPose

src/robot_control/src/BallPosePub.py
@@ -40,8 +40,8 @@ class ball_pose:
         self.odom_sub = rospy.Subscriber("odom",
             Odometry, self.callback_odom,  queue_size = 1)
 
-        self.camera_x = 0.125
-        self.camera_y = 0
+        self.camera_x = 0.100
+        self.camera_y = 0.150
         
         self.ball_srv = rospy.Service('/ball_pose_srv', BallPose, self.handle_ball_pose)
         # self.ballpose_pub = rospy.Publisher('/ball_pose_pub', Pose2D,  queue_size=10)
@@ -61,11 +61,23 @@ class ball_pose:
 
         if self.odom:
             self.ballpose_cal = True # semaphore status 
-            theta_right = -1*self.odom_pose.theta
-            self.ball_pose.x = self.odom_pose.x  + math.cos(theta_right)*(self.camera_x + self.ball_dist)
-            self.ball_pose.y = self.odom_pose.y  + math.sin(theta_right)*(self.camera_y + self.ball_dist)
+            theta_right = self.odom_pose.theta
+            self.ball_pose.x = self.odom_pose.x  + math.cos(math.pi/2 + theta_right)*(self.camera_y + self.ball_dist)
+            self.ball_pose.y = self.odom_pose.y  + math.sin(math.pi/2 + theta_right)*(self.camera_y + self.ball_dist)
             self.ball_pose.theta = 0
             self.ballpose_cal = False
+            rospy.loginfo("**************ball pose****************")
+            rospy.loginfo(self.ball_pose.x)
+            rospy.loginfo(self.ball_pose.y)
+            rospy.loginfo("*************odom_poses*****************")
+            rospy.loginfo(self.odom_pose.x)
+            rospy.loginfo(self.odom_pose.y)
+
+            rospy.loginfo("*************shitty things*****************")
+            rospy.loginfo(math.cos(theta_right)*(self.camera_x + self.ball_dist))
+            rospy.loginfo(math.sin(theta_right)*(self.camera_x + self.ball_dist))
+            rospy.loginfo(self.odom_pose.theta)
+
 
         print("outside the srv block")
         resp = BallPoseResponse()
@@ -84,8 +96,6 @@ class ball_pose:
             quaternion = odom_data.pose.pose.orientation
             explicit_quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
             euler = tf.transformations.euler_from_quaternion(explicit_quat)
-
-            rospy.loginfo(euler[2])
             self.odom_pose.theta = euler[2]
 
 def main(args):